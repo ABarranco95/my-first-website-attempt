de3e3c8e5827187e4bb26dc6b868184584355f5e
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-10-28 07:00:39 -0700

    [app.js] Attempt to creat new element using javasscript

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// const navbarTab = document.createElement('ul')
+
+// /* <ul class="navbar-nav">
+//         <li class="nav-item active">
+//             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+//         </li>
+//     </ul> */
+
+
+// const ulElement = document.createElement('ul');
+// divElement.classList.add('navbar-nav');
+
+
+// const link = document.createElement('a');
+
